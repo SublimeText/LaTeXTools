b251a0aa678ff07ee8251cee9e8463c1caed085d
ms4py <marc.schlaich@googlemail.com>
2012-05-25 09:39:10 +0200

    more universal solution for getting the root document

--- getTeXRoot.py
+++ getTeXRoot.py
@@ -1,28 +1,31 @@
-import os.path, re
+import glob
+import os
+import re
 
-# Parse magic comments to retrieve TEX root
-# Stops searching for magic comments at first non-comment line of file
-# Returns root file or current file
+MAIN_PATTERN = re.compile(r'\\begin{document}')
 
-# Contributed by Sam Finn
 
-def get_tex_root(texFile):
-	for line in open(texFile, "rU").readlines():
-		if not line.startswith('%'):
-			root = texFile
-			break
-		else:
-			# We have a comment match; check for a TEX root match
-			if re.match(r"%\s*!TEX\s+root *= *",line):
-				# we have a TEX root match 
-				# Break the match into path, file and extension
-				# Create TEX root file name
-				# If there is a TEX root path, use it
-				# If the path is not absolute and a src path exists, pre-pend it
-				mroot = re.match(r"%\s*!TEX\s+root *= *(.*tex)\s*$",line)
-				(texPath, texName) = os.path.split(texFile)
-				(rootPath, rootName) = os.path.split(mroot.group(1))
-				root = os.path.join(texPath,rootPath,rootName)
-				root = os.path.normpath(root)
-				break
-	return root
+def is_main(tex_file):
+    with open(tex_file) as fobj:
+        for line in fobj:
+            if MAIN_PATTERN.search(line):
+                return True
+
+
+def get_tex_root(tex_file):
+    if is_main(tex_file):
+        return tex_file
+
+    path, _ = os.path.split(tex_file)
+    path = os.path.abspath(path)
+    while True:
+        for fname in glob.iglob(os.path.join(path, '*.tex')):
+            if fname == tex_file:
+                continue
+            if is_main(fname):
+                return fname
+
+        parent_path = os.path.abspath(os.path.join(path, '..'))
+        if path == parent_path:
+            raise ValueError('Main tex file not found.')
+        path = parent_path
