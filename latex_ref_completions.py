ab0da5d8e63b4ba05e7f518a419721224ec4c645
Marciano Siniscalchi <msiniscalchi@gmail.com>
2012-09-13 22:03:06 -0500

    Ref completion: minor fix and first stab at ref command (quick panel)

--- latex_ref_completions.py
+++ latex_ref_completions.py
@@ -127,7 +127,9 @@ class LatexRefCompletions(sublime_plugin.EventListener):
 
         completions = []
         # stop matching at FIRST } after \label{
-        view.find_all(r'\\label\{([^\{\}]+)\}', 0, '\\1', completions)
+        # I think we don't need to match here, as getTeXroot returns our file name
+        # if there is no TEX root directive
+        # view.find_all(r'\\label\{([^\{\}]+)\}', 0, '\\1', completions)
 
         root = getTeXRoot.get_tex_root(view)
 
@@ -139,3 +141,128 @@ class LatexRefCompletions(sublime_plugin.EventListener):
         r = [(label + "\t\\ref{}", label + post_snippet) for label in completions]
         #print r
         return (r, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+
+
+### Ref completions using the quick panel
+
+class LatexRefCommand(sublime_plugin.TextCommand):
+
+    # Remember that this gets passed an edit object
+    def run(self, edit):
+        # get view and location of first selection, which we expect to be just the cursor position
+        view = self.view
+        point = view.sel()[0].b
+        print point
+        # Only trigger within LaTeX
+        # Note using score_selector rather than match_selector
+        if not view.score_selector(point,
+                "text.tex.latex"):
+            return
+
+        # Get the contents of the current line, from the beginning of the line to
+        # the current point
+        line = view.substr(sublime.Region(view.line(point).a, point))
+        print line
+
+                # Reverse, to simulate having the regex
+        # match backwards (cool trick jps btw!)
+        line = line[::-1]
+        #print line
+
+        # Check the first location looks like a ref, but backward
+        rex = re.compile("([^_]*_)?(p)?fer(qe)?")
+        expr = match(rex, line)
+        # print expr
+
+        if expr:
+            # Return the matched bits, for mangling
+            prefix, has_p, has_eq = rex.match(expr).groups()
+            preformatted = False
+            if prefix:
+                prefix = prefix[::-1]   # reverse
+                prefix = prefix[1:]     # chop off #
+            else:
+                prefix = ""
+            #print prefix, has_p, has_eq
+
+        else:
+            # Check to see if the location matches a preformatted "\ref{blah"
+            rex = re.compile(r"([^{}]*)\{fer(qe)?\\(\()?")
+            expr = match(rex, line)
+
+            if not expr:
+                return []
+
+            preformatted = True
+            # Return the matched bits (barely needed, in this case)
+            prefix, has_eq, has_p = rex.match(expr).groups()
+            if prefix:
+                prefix = prefix[::-1]   # reverse
+            else:
+                prefix = ""
+            #print prefix, has_p, has_eq
+
+        if has_p:
+            pre_snippet = "(\\ref{"
+            post_snippet = "})"
+        elif has_eq:
+            pre_snippet = "\\eqref{"
+            post_snippet = "}"
+        else:
+            pre_snippet = "\\ref{"
+            post_snippet = "}"
+
+        if not preformatted:
+            # Replace ref_blah with \ref{blah
+            expr_region = sublime.Region(point - len(expr), point)
+            #print expr[::-1], view.substr(expr_region)
+            ed = view.begin_edit()
+            view.replace(ed, expr_region, pre_snippet + prefix)
+            # save prefix begin and endpoints points
+            new_point_a = point - len(expr) + len(pre_snippet)
+            new_point_b = new_point_a + len(prefix)
+            view.end_edit(ed)
+
+        else:
+            # Don't include post_snippet if it's already present
+            suffix = view.substr(sublime.Region(point, point + len(post_snippet)))
+            new_point_a = point - len(prefix)
+            new_point_b = point
+            if post_snippet == suffix:
+                post_snippet = ""
+
+        completions = []
+
+        root = getTeXRoot.get_tex_root(view)
+
+        print "TEX root: " + root
+        find_labels_in_files(os.path.dirname(root), root, completions)
+        # remove duplicate bib files
+        completions = list(set(completions))
+        # filter! Note matching is "less fuzzy" than ST2. Room for improvement...
+        completions = [c for c in completions if prefix in c]
+
+        if not completions:
+            sublime.error_message("No label matches %s !" % (prefix,))
+            return
+
+        # Note we now generate refs on the fly. Less copying of vectors! Win!
+        def on_done(i):
+            print "latex_ref_completion called with index %d" % (i,)
+            
+            # Allow user to cancel
+            if i<0:
+                return
+
+            ref = completions[i] + post_snippet
+            
+
+            print "selected %s" % completions[i] 
+            # Replace ref expression with reference and possibly post_snippet
+            expr_region = sublime.Region(new_point_a,new_point_b)
+            ed = view.begin_edit()
+            view.replace(ed, expr_region, ref)
+            view.end_edit(ed)
+        
+        view.window().show_quick_panel(completions, on_done)
+ 
