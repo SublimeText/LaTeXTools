b1ea8300fa09e020c0df925305a409a814039fa2
Marciano Siniscalchi <msiniscalchi@gmail.com>
2012-10-14 21:39:38 -0500

    Rewrite latex{Command,Environment}.py so starred cmds/envs are OK

--- latexEnvironment.py
+++ latexEnvironment.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin
+import re
 
 # Insert LaTeX environment based on current word
 # Position cursor inside environment
@@ -6,8 +7,22 @@ import sublime, sublime_plugin
 class latexenvCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
 		view = self.view
-		currword = view.word(view.sel()[0])
-		command = view.substr(currword)
-		view.erase(edit, currword)
-		snippet = "\\\\begin{" + command + "}\n$1\n\\\\end{" + command + "}$0"
-		view.run_command("insert_snippet", {'contents' : snippet})
+
+		# Workaround: env* and friends trip ST2 up because * is a word boundary,
+		# so we search for a word boundary
+
+		# Code is similar to latex_cite_completions.py (should prbly factor out)
+		point = view.sel()[0].b
+		line = view.substr(sublime.Region(view.line(point).a, point))
+		line = line[::-1]
+		rex = re.compile(r"(\S*)\s?")
+		expr = re.match(rex, line)
+		if expr:
+			environment = expr.group(1)[::-1]
+			environment_region = sublime.Region(point-len(environment),point)
+			view.erase(edit, environment_region)
+			snippet = "\\\\begin{" + environment + "}\n$1\n\\\\end{" + environment + "}$0"
+			view.run_command("insert_snippet", {'contents' : snippet})
+		else:
+			sublime.status_message("LATEXTOOLS INTERNAL ERROR: could not find environment to expand")
+
