678c2c0ceef45a6e66b40bdf405fa0db05688fd3
Ulrich Gabor <ulrichgabor@Ulrichs-MacBook-Air.local>
2013-01-11 13:32:51 +0100

    Command to remove temporary files

---
+++ delete_temp_files.py
@@ -0,0 +1,26 @@
+import sublime, sublime_plugin
+import os
+import getTeXRoot
+
+class Delete_temp_filesCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		# Retrieve file and dirname.
+		view = self.window.active_view()
+		self.file_name = getTeXRoot.get_tex_root(view)
+		if not os.path.isfile(self.file_name):
+			sublime.error_message(self.file_name + ": file not found.")
+			return
+
+		self.tex_base, self.tex_ext = os.path.splitext(self.file_name)
+
+
+		# Delete the files.
+		temp_exts = ['.blg','.bbl','.aux','.log','.brf','.nlo','.out','.dvi','.ps',
+			'.lof','.toc','.fls','.fdb_latexmk','.pdfsync','.synctex.gz','.ind','.ilg','.idx']
+
+		for temp_ext in temp_exts:
+			file_name_to_del = self.tex_base + temp_ext
+			#print file_name_to_del
+			if os.path.exists(file_name_to_del):
+				#print ' deleted '
+				os.remove(file_name_to_del)
