a22a9a8a7dc00343d8386612721a1c9072d3cfd1
Marciano Siniscalchi <msiniscalchi@gmail.com>
2013-06-22 12:27:22 -0500

    Robustify latexCommand

--- latexCommand.py
+++ latexCommand.py
@@ -15,13 +15,16 @@ class latexcmdCommand(sublime_plugin.TextCommand):
 		point = view.sel()[0].b
 		line = view.substr(sublime.Region(view.line(point).a, point))
 		line = line[::-1]
-		rex = re.compile(r"([^\s\{]*)\s?\{?")
+		# Stop at space, {,[,( or $
+		rex = re.compile(r"([^\s\{\[\(\$]*)\s?\{?")
 		expr = re.match(rex, line)
 		if expr:
 			command = expr.group(1)[::-1]
 			command_region = sublime.Region(point-len(command),point)
 			view.erase(edit, command_region)
-			snippet = "\\\\" + command + "{$1} $0"
+			# Be forgiving and skip \ if the user provided one (by mistake...)
+			bslash = "" if command[0] == '\\' else "\\\\" 
+			snippet = bslash + command + "{$1} $0"
 			view.run_command("insert_snippet", {'contents': snippet})
 		else:
 			sublime.status_message("LATEXTOOLS INTERNAL ERROR: could not find command to expand")
