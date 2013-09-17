4add640d00cf19b9ddc507f39f4ca563643aec92
Marciano Siniscalchi <msiniscalchi@gmail.com>
2013-09-17 15:17:26 -0500

    Engine selection in traditional builder

--- builders/traditionalBuilder.py
+++ builders/traditionalBuilder.py
@@ -9,6 +9,7 @@ else:
 
 from pdfBuilder import PdfBuilder
 import sublime_plugin
+import re
 import os, os.path
 import codecs
 
@@ -46,12 +47,23 @@ class TraditionalBuilder(PdfBuilder):
 		self.cmd = prefs.get("command", default_command)
 		# Default tex engine (pdflatex if none specified)
 		self.engine = prefs.get("program", "pdflatex")
-		# Sanity check: if "strange" engine, default to pdflatex but give message
+		# Sanity check: if "strange" engine, default to pdflatex (silently...)
 		if not(self.engine in ['pdflatex', 'xelatex', 'lualatex']):
 			self.engine = 'pdflatex'
-			sublime.error_message("Unknown engine specified in preferences;\n" \
-								  "defaulting to pdflatex.")
+
+
+
+	#
+	# Very simple here: we yield a single command
+	# Only complication is handling custom tex engines
+	#
+	def commands(self):
+		# Print greeting
+		self.display("\n\nTraditionalBuilder: ")
+
 		# See if the root file specifies a custom engine
+		engine = self.engine
+		cmd = self.cmd[:] # Warning! If I omit the [:], cmd points to self.cmd!
 		for line in codecs.open(self.tex_root, "r", "UTF-8", "ignore").readlines():
 			if not line.startswith('%'):
 				break
@@ -60,27 +72,21 @@ class TraditionalBuilder(PdfBuilder):
 				mroot = re.match(r"%\s*!TEX\s+(?:TS-)?program *= *(xelatex|lualatex|pdflatex)\s*$",line)
 				if mroot:
 					# Sanity checks
-					if "texify" == self.cmd[0]:
-						sublime.error_message("Sorry, cannot select engine using a %!TEX program directive on MikTeX.\n"\
-											  "Running with default engine.")
-						break 
-					if not ("$pdflatex\s?=\s?'%E" in self.cmd[3]): # fixup blanks (linux)
-						sublime.error_message("You are using a custom LaTeX.sublime-build file (in User maybe?). Cannot select engine using a %!TEX program directive.")
-						break
-					self.engine = mroot.group(1)
+					if "texify" == cmd[0]:
+						sublime.error_message("Sorry, cannot select engine using a %!TEX program directive on MikTeX.\n")
+						yield("", "Could not compile.")
+					if not re.match(r"\$pdflatex\s?=\s?'%E", cmd[3]): # fixup blanks (linux)
+						sublime.error_message("You are using a custom build command.\n"\
+							"Cannot select engine using a %!TEX program directive.\n")
+						yield("", "Could not compile.")
+					engine = mroot.group(1)
 					break
-		if self.engine != 'pdflatex': # Since pdflatex is standard, we do not output a msg. for it.
-			self.output("Using engine " + self.engine + "\n")
-		self.cmd[3] = self.cmd[3].replace("%E", self.engine)
+		if engine != self.engine:
+			self.display("Engine: " + self.engine + " -> " + engine + ". ")
+			
+		cmd[3] = cmd[3].replace("%E", engine)
 
-	#
-	# Very simple here: we yield a single command
-	#
-	def commands(self):
-		# Print greeting
-		self.display("\n\nTraditionalBuilder: ")
-
-		yield (self.cmd + [self.base_name], "invoking " + self.cmd[0] + "... ")
+		yield (cmd + [self.base_name], "Invoking " + cmd[0] + "... ")
 
 		self.display("done.\n")
 		
