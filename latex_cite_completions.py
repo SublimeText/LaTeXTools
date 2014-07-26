ecb5a7d197bafd9a9d5245acdda068b09f126de3
183amir <183.amir@gmail.com>
2014-07-26 17:33:27 +0300

    added a small support for custom .bib files
    https://github.com/SublimeText/LaTeXTools/issues/376

--- latex_cite_completions.py
+++ latex_cite_completions.py
@@ -72,6 +72,7 @@ def find_bib_files(rootdir, src, bibfiles):
 
     bibtags =  re.findall(r'\\bibliography\{[^\}]+\}', src_content)
     bibtags += re.findall(r'\\addbibresource\{[^\}]+.bib\}', src_content)
+    bibtags += re.findall(r'\\bibfiles\{[^\}]+\}', src_content)
 
     # extract absolute filepath for each bib file
     for tag in bibtags:
