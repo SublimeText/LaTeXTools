3c1130223d6a791fe7042be12fe4ea99d102b874
Christoph Hösler <christoph.hoesler@gmail.com>
2014-08-19 15:02:44 +0200

    Added options part to addbibresource regex
    \addbibresource accepts options

--- latex_cite_completions.py
+++ latex_cite_completions.py
@@ -71,7 +71,7 @@ def find_bib_files(rootdir, src, bibfiles):
                 f.close()
 
     bibtags =  re.findall(r'\\bibliography\{[^\}]+\}', src_content)
-    bibtags += re.findall(r'\\addbibresource\{[^\}]+.bib\}', src_content)
+    bibtags += re.findall(r'\\addbibresource(?:\[[^\]]*\])?\{[^\}]+\.bib\}', src_content)
 
     # extract absolute filepath for each bib file
     for tag in bibtags:
