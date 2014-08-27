7fbf8daaf222656a2fe8b684840248e69f7e5e2a
Ian <ig0774@gmail.com>
2014-08-27 20:15:37 +0100

    Ugly hack so latex_command_grammar works on ST2

--- latex_commands_grammar.py
+++ latex_commands_grammar.py
@@ -14,9 +14,35 @@ latex_command   <<= (Suppress(Literal(u'\\')) + Suppress(CharsNotIn(u'{')) + bra
 def remove_latex_commands(s):
     result = latex_command.scanString(s)
     if result:
-        for r in result:
-            tokens, preloc, nextloc = r
-            s = s[:preloc] \
-                + u''.join(tokens) \
-                + s[nextloc:]
+        try:
+            for r in result:
+                tokens, preloc, nextloc = r
+                s = s[:preloc] \
+                    + u''.join(tokens) \
+                    + s[nextloc:]
+        except TypeError:
+            # Note: for some reason this grammar will occassionally produce a TypeError on the first
+            # run-through only and only on ST2; this error is not reproducible in a simulated environment
+            # running Python 2.6.8, so I've resorted to this ugly hack which, in the case of the type error on
+            # ST2 will re-process the string; for whatever reason, the error doesn't appear in the second 
+            # and subsequent invocations of this grammar.
+            import sublime
+            import sys
+
+            # save the current exception state
+            exc_info = sys.exc_info()
+
+            if sublime.version < '3000':
+                # check that the caller is not this function to avoid an infinite recursion
+                import inspect
+                frame = inspect.currentframe()
+                try:
+                    if frame.f_back.f_code.co_name != 'remove_latex_commands':
+                        remove_latex_commands(s)
+                    else:
+                        raise exc_info[0], exc_info[1], exc_info[2]
+                finally:
+                    del frame
+            else:
+                raise exc_info[0], exc_info[1], exc_info[2]
     return s
