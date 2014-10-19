d1091cd595a594406b815d02513d8b855b3387c4
Rafael Lerm <rafaellerm@gmail.com>
2014-10-19 19:23:30 -0200

    Support for multi-line/spaces in \bibliography command.

--- latex_cite_completions.py
+++ latex_cite_completions.py
@@ -77,6 +77,7 @@ def find_bib_files(rootdir, src, bibfiles):
     for tag in bibtags:
         bfiles = re.search(r'\{([^\}]+)', tag).group(1).split(',')
         for bf in bfiles:
+            bf = bf.strip()
             if bf[-4:].lower() != '.bib':
                 bf = bf + '.bib'
             # We join with rootdir - everything is off the dir of the master file
