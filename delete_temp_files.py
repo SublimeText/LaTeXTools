e3cf49a2957f91bfbfebaaa8221b77653d807c93
balu <balu@mobile>
2015-04-17 14:18:24 +0200

    Delete Temp files with double extension (like .synctex.gz)

--- delete_temp_files.py
+++ delete_temp_files.py
@@ -30,10 +30,10 @@ class Delete_temp_filesCommand(sublime_plugin.WindowCommand):
 
 		for dir_path, dir_names, file_names in os.walk(self.path):
 			for file_name in file_names:
-				file_base, file_ext = os.path.splitext(file_name)
-				if file_ext in temp_exts:
-					file_name_to_del = os.path.join(dir_path, file_name)
-					if os.path.exists(file_name_to_del):
-						os.remove(file_name_to_del)
+				for ext in temp_exts:
+					if file_name.endswith(ext):
+						file_name_to_del = os.path.join(dir_path, file_name)
+						if os.path.exists(file_name_to_del):
+							os.remove(file_name_to_del)
 
 		sublime.status_message("Deleted temp files")
