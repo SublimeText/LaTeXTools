2086515ecde24d0398ad2f4730f4b0dc1f227e43
Ian <ig0774@gmail.com>
2015-04-28 20:17:42 +0100

    Changes to address #498. Now launches kpsewhich using the texpath setting from LaTeXTools.sublime-settings.

--- kpsewhich.py
+++ kpsewhich.py
@@ -1,15 +1,61 @@
+from __future__ import print_function
+import subprocess
 from subprocess import Popen, PIPE
+import os
+import sublime
+
+if sublime.version() < '3000':
+    _ST3 = False
+else:
+    _ST3 = True
+
+__all__ = ['kpsewhich']
+
+def get_texpath():
+    settings = sublime.load_settings('LaTeXTools.sublime-settings')
+    platform_settings = settings.get(sublime.platform())
+    texpath = platform_settings['texpath']
+
+    if not _ST3:
+        return os.path.expandvars(texpath).encode(sys.getfilesystemencoding())
+    else:
+        return os.path.expandvars(texpath)
 
 def kpsewhich(filename, file_format=None):
+    # build command
+    command = ['kpsewhich']
+    if file_format is not None:
+        command.append('-format=%s' % (file_format))
+    command.append(filename)
+
+    # setup the environment to run the subprocess in
+    texpath = get_texpath() or os.environ['PATH']
+    env = dict(os.environ)
+    env['PATH'] = texpath
+
     try:
-        if file_format is None or type(file_format) != type(''):
-            p = Popen(['kpsewhich', filename], stdout=PIPE, stdin=PIPE)
-        else:
-            p = Popen(['kpsewhich', '-format=%s' % (file_format), filename], stdout=PIPE, stdin=PIPE)
-        path = p.communicate()[0].rstrip()
+        # Windows-specific adjustments
+        startupinfo = None
+        shell = False
+        if sublime.platform() == 'windows':
+            # ensure console window doesn't show
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            shell = True
+            
+        print('Running %s' % (' '.join(command)))
+        p = Popen(
+            command, 
+            stdout=PIPE,
+            stdin=PIPE,
+            startupinfo=startupinfo,
+            shell=shell,
+            env=env
+        )
+        path = p.communicate()[0].decode('utf-8').rstrip()
         if p.returncode == 0:
             return path
     except OSError:
-        # i.e., kpsewhich cannot be found
-        pass
+        sublime.error_message('Could not run kpsewhich. Please ensure that your texpath setting is configured correctly in the LaTeXTools settings.')
+
     return None
