c3e7fdfb640320cac2455308f20a1c01e687ed63
Ian <ig0774@gmail.com>
2015-06-14 06:16:45 +0100

    Set syntax for buffer on save; also provide an option to turn off the syntax-setting behaviour

--- texSyntaxListener.py
+++ texSyntaxListener.py
@@ -9,11 +9,22 @@ LATEX_SYNTAX = 'Packages/LaTeX/LaTeX.tmLanguage'
 
 class TeXSyntaxListener(sublime_plugin.EventListener):
     def on_load(self, view):
+        self.detect_and_apply_syntax(view)
+
+    def on_post_save(self, view):
+        self.detect_and_apply_syntax(view)
+
+    def detect_and_apply_syntax(self, view):
         if view.is_scratch() or not view.file_name():
             return
 
         current_syntax = view.settings().get('syntax')
         if current_syntax == LATEX_SYNTAX:
+            return
+
+        global_settings = view.load_settings('LaTeXTools.sublime-settings')
+        if not view.settings.get('latextools_set_syntax',
+            global_settings.get('latextools_set_syntax', True)):
             return
 
         file_name = view.file_name()
