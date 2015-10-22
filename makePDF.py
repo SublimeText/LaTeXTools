6fa22ecd15a5ac54378e722c5f652709ed52cb17
Richard Stein <r-stein.github@web.de>
2015-10-22 12:08:06 +0200

    Added a setting to hide the build panel
    Now it is possible to hide the build panel after build
    it can be configured when it should be hidden

--- makePDF.py
+++ makePDF.py
@@ -184,6 +184,24 @@ class CmdThread ( threading.Thread ):
 				content.extend(warnings)
 			else:
 				content.append("")
+
+			s = sublime.load_settings("LaTeXTools.sublime-settings")
+			hide_panel_level = s.get("hide_build_panel")
+			hide_panel = {
+				"always": True,
+				"no_errors": not errors,
+				"no_warnings": not errors and not warnings,
+				"never": False
+			}.get(hide_panel_level, False)
+			if hide_panel:
+				self.caller.window.run_command("hide_panel", {"panel": "output.exec"})
+				message = "build completed"
+				if errors:
+					message += " with errors"
+				if warnings:
+					message += " and" if errors else " with"
+					message += " warnings"
+				sublime.status_message(message)
 		except Exception as e:
 			content=["",""]
 			content.append("LaTeXtools could not parse the TeX log file")
