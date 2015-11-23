97c8f5d0c9db3f3e4661439eed2abf3873aa7525
Richard Stein <r-stein.github@web.de>
2015-11-23 20:09:51 +0100

    Added a `cache` function, which combines read and write

--- latextools_utils/cache.py
+++ latextools_utils/cache.py
@@ -27,6 +27,25 @@ def delete_local_cache(tex_root):
         shutil.rmtree(cache_path)
 
 
+def cache(tex_root, name, generate):
+    """
+    Alias for cache_local:
+    Uses the local cache to retrieve the entry for the name.
+    If the entry is not available, it will be calculated via the
+    generate-function and cached using pickle.
+    The local cache is per tex document and the path will extracted
+    from the tex root.
+
+    Arguments:
+    tex_root -- the root of the tex file (for the folder of the cache)
+    name -- the relative file name to write the object
+    generate -- a function pointer/closure to create the cached object
+        for case it is not available in the cache,
+        must be compatible with pickle
+    """
+    return cache_local(tex_root, name, generate)
+
+
 def write(tex_root, name, obj):
     """
     Alias for write_local:
@@ -60,6 +79,29 @@ def read(tex_root, name):
     return read_local(tex_root, name)
 
 
+def cache_local(tex_root, name, generate):
+    """
+    Uses the local cache to retrieve the entry for the name.
+    If the entry is not available, it will be calculated via the
+    generate-function and cached using pickle.
+    The local cache is per tex document and the path will extracted
+    from the tex root.
+
+    Arguments:
+    tex_root -- the root of the tex file (for the folder of the cache)
+    name -- the relative file name to write the object
+    generate -- a function pointer/closure to create the cached object
+        for case it is not available in the cache,
+        must be compatible with pickle
+    """
+    try:
+        result = read_local(tex_root, name)
+    except CacheMiss:
+        result = generate()
+        write_local(tex_root, name, result)
+    return result
+
+
 def write_local(tex_root, name, obj):
     """
     Writes the object to the local cache using pickle.
@@ -91,6 +133,27 @@ def read_local(tex_root, name):
     """
     cache_path = _local_cache_path(tex_root)
     return _read(cache_path, name)
+
+
+def cache_global(tex_root, name, generate):
+    """
+    Uses the global sublime cache retrieve the entry for the name.
+    If the entry is not available, it will be calculated via the
+    generate-function and cached using pickle.
+
+    Arguments:
+    tex_root -- the root of the tex file (for the folder of the cache)
+    name -- the relative file name to write the object
+    generate -- a function pointer/closure to create the cached object
+        for case it is not available in the cache,
+        must be compatible with pickle
+    """
+    try:
+        result = read_global(tex_root, name)
+    except CacheMiss:
+        result = generate()
+        write_global(tex_root, name, result)
+    return result
 
 
 def write_global(name, obj):
