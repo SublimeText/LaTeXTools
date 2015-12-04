82269c21c4149df9b3184434db6b2daac6653bb7
Richard Stein <r-stein.github@web.de>
2015-12-04 09:36:36 +0100

    Improved soundness of jump to cite

--- jumpto_anywhere.py
+++ jumpto_anywhere.py
@@ -32,7 +32,7 @@ INPUT_REG_EXPS = [INPUT_REG, BIB_REG, IMAGE_REG]
 COMMAND_REG = re.compile(
     r"\\(?P<command>[\w]+)"
     r"(?:\[[^\]]*\])?"
-    r"\{(?P<args>[^}]+)\}",
+    r"\{(?P<args>[^}]*)\}",
     re.UNICODE
 )
 
@@ -57,14 +57,14 @@ def _jumpto_ref(view, com_reg):
 
 def _jumpto_cite(view, com_reg):
     tex_root = get_tex_root(view)
-    if tex_root is None:
+    bib_key = com_reg.group("args")
+    if tex_root is None or not bib_key:
         return
     base_dir = os.path.dirname(tex_root)
     ana = analysis.get_analysis(tex_root)
 
     bib_commands = ana.filter_commands(
        ["bibliography", "nobibliography", "addbibresource"])
-    bib_key = com_reg.group("args")
     for bib_command in bib_commands:
         for bib_file in jumpto_tex_file._split_bib_args(bib_command.args):
             if not os.path.splitext(bib_file)[1]:
@@ -73,13 +73,15 @@ def _jumpto_cite(view, com_reg):
             try:
                 file_content = utils.read_file_unix_endings(bib_file)
                 start = file_content.find(bib_key)
-                if start == -1:
+                # check that we found the entry and we are not inside a word
+                if start == -1 or file_content[start-1:start].isalnum() or\
+                        file_content[start:start+1].isalnum():
                     continue
                 region = sublime.Region(start, start + len(bib_key))
                 utils.open_and_select_region(view, bib_file, region)
                 return
             except Exception as e:
-                print("Error occured opening file {0}".format(bib_file))
+                print("Error occurred opening file {0}".format(bib_file))
                 print(e)
                 continue
 
