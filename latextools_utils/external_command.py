5090e61c5e157b48d65520b38b0f6fe7cf4249a0
Ian <ig0774@gmail.com>
2016-11-10 17:22:50 +0000

    Fixes #930
    Basically, CreateProcess doesn't use the PATH passed to the process to determine which executable to run. To work around this, we use which() to find the full path of the exe and pass that in. The created process has the PATH set correctly at that point.

--- latextools_utils/external_command.py
+++ latextools_utils/external_command.py
@@ -46,9 +46,11 @@ from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 
 try:
     from latextools_utils.settings import get_setting
+    from latextools_utils.system import which
     from latextools_utils.utils import run_on_main_thread
 except ImportError:
     from .settings import get_setting
+    from .system import which
     from .utils import run_on_main_thread
 
 if sys.version_info < (3,):
@@ -172,6 +174,11 @@ def external_command(command, cwd=None, shell=False, env=None,
 
         if show_window:
             startupinfo.wShowWindow = 1
+
+        if not os.path.isabs(command[0]):
+            command[0] = which(
+                command[0], path=_env['PATH'] or os.environ['PATH']
+            )
 
         # encode cwd in the file system encoding; this is necessary to support
         # some non-ASCII paths; see PR #878. Thanks to anamewing for the
