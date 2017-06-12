34e8e4b9d4833765ce1cc89f399935f207285350
Richard Stein <r-stein.github@web.de>
2017-06-12 17:53:42 +0200

    Changed the smart paste command to also support offline images
    This command now checks whether the clipboard contains an url or an file path. If it contains a file path the image will be copied at the corresponding position

--- smart_paste.py
+++ smart_paste.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import threading
 import urllib.request
 
@@ -35,13 +36,25 @@ class LatextoolsDownloadInsertImageHelperCommand(sublime_plugin.TextCommand):
                 if on_done:
                     on_done()
 
-    def run(self, edit, image_name, image_url, image_ext):
+    @staticmethod
+    def _copy(new_image_path, old_image_path, on_done=None):
+        # create the folder
+        os.makedirs(os.path.split(new_image_path)[0], exist_ok=True)
+        shutil.copy2(old_image_path, new_image_path)
+        if on_done:
+            on_done()
+
+    def run(self, edit, image_name, image_url, image_ext, offline):
         view = self.view
         tex_root = get_tex_root(view)
         if not tex_root:
             return
         root_dir, _ = os.path.split(tex_root)
-        image_path = os.path.normpath(os.path.join(root_dir, image_name))
+        if os.path.isabs(image_name):
+            image_path = image_name
+        else:
+            image_path = os.path.join(root_dir, image_name)
+        image_path = os.path.normpath(image_path)
         image_ext = "." + image_ext
         if not image_path.endswith(image_ext):
             image_path += image_ext
@@ -50,20 +63,31 @@ class LatextoolsDownloadInsertImageHelperCommand(sublime_plugin.TextCommand):
         contents = template.replace("<<image_name>>", image_name)
         self.view.run_command("insert_snippet", {"contents": contents})
 
-        pid = view.add_phantom(
-            "lt-dl-img", sublime.Region(pos), "Downloading Image...",
-            sublime.LAYOUT_BLOCK)
+        if offline:
+            # if it is offline just copy the file non-blocking
+            threading.Thread(
+                target=self._copy,
+                args=(image_path, image_url)
+            ).start()
+        else:
+            # if the image is online also show a phantom to notify the
+            # download
+            pid = view.add_phantom(
+                "lt-dl-img", sublime.Region(pos), "Downloading Image...",
+                sublime.LAYOUT_BLOCK)
 
-        def on_done():
-            view.erase_phantom_by_id(pid)
-        threading.Thread(
-            target=self._download,
-            args=(image_path, image_url, on_done)
-        ).start()
+            def on_done():
+                view.erase_phantom_by_id(pid)
+
+            threading.Thread(
+                target=self._download,
+                args=(image_path, image_url, on_done)
+            ).start()
 
 
-def download_insert_image(window, view, image_url):
-    if not get_tex_root(view):
+def download_insert_image(window, view, image_url, offline=False):
+    tex_root = get_tex_root(view)
+    if not tex_root:
         window.run_command("paste")
         sublime.status_message(
             "Need to save the view before downloading image.")
@@ -71,31 +95,63 @@ def download_insert_image(window, view, image_url):
     ext = image_url.split(".")[-1]
     caption = "Image name (.{})".format(ext)
 
-    def on_cancel(text):
+    def on_cancel():
         window.run_command("paste")
 
     def on_done(text):
         kwargs = {
             "image_name": text,
             "image_url": image_url,
-            "image_ext": ext
+            "image_ext": ext,
+            "offline": offline,
         }
         window.run_command("latextools_download_insert_image_helper", kwargs)
 
+    # if the image is offline check whether it is in a subdirectory
+    # if the root folder. If so just include it directly
+    if offline:
+        root_dir, _ = os.path.split(tex_root)
+        image_dir = os.path.normpath(os.path.split(image_url)[0])
+        if image_dir.startswith(root_dir):
+            image_insert = os.path.relpath(image_url, root_dir)
+            image_insert = image_insert.replace("\\", "/")
+            contents = template.replace("<<image_name>>", image_insert)
+            view.run_command("insert_snippet", {"contents": contents})
+            return
+
+    example_name = ""
+    if not offline:
+        example_name = image_url.split("/")[-1]
+    else:
+        _, example_name = os.path.split(image_url)
+
     window.show_input_panel(
-        caption, "", on_done, on_change=lambda _: None,
+        caption, example_name, on_done, on_change=lambda _: None,
         on_cancel=on_cancel)
+    window.run_command("select_all")
+
+
+def _is_possible_image_path(text):
+    return any(
+        text.endswith("." + image_type)
+        for image_type in get_setting("image_types")
+    )
 
 
 class LatextoolsSmartPasteCommand(sublime_plugin.WindowCommand):
     def run(self):
         window = self.window
         view = window.active_view()
-        clipboard = sublime.get_clipboard().strip()
+        clipboard = sublime.get_clipboard()
 
-        if (url_regex.match(clipboard) and
-            any(clipboard.endswith(image_type)
-                for image_type in get_setting("image_types"))):
-            download_insert_image(window, view, clipboard)
+        content = clipboard.strip()
+        if sublime.platform() == "windows":
+            content = content.strip('"')
+
+        maybe_image = _is_possible_image_path(content)
+        if maybe_image and url_regex.match(content):
+            download_insert_image(window, view, content)
+        elif maybe_image and os.path.isfile(content):
+            download_insert_image(window, view, content, offline=True)
         else:
             window.run_command("paste")
