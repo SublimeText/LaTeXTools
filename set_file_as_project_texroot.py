052ed568776ed327a10d58e6d13ce32613074de7
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2020-12-04 18:36:49 +0100

    fix of overwriting

--- set_file_as_project_texroot.py
+++ set_file_as_project_texroot.py
@@ -8,12 +8,13 @@ class SetFileAsProjectTexrootCommand(sublime_plugin.TextCommand):
 		current_project_name = sublime.expand_variables("$project_name",variables)
 
 		project_data = self.view.window().project_data()
+		project_settings = project_data["settings"]
 
 		#print("current file's name: ", current_file_name)
 		#print("current project's name: ", current_project_name)
-		#print("project data previously:", project_data)
+		#print("project settings previously:", project_settings)
 		
-		project_data.update({'settings': {'TEXroot': current_file_name} })
+		project_data["settings"].update({'TEXroot': current_file_name})
 		self.view.window().set_project_data(project_data)
 
 		#print("project data currently:", self.view.window().project_data())
