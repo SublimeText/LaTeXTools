e08894f5d74f1068bd35a52ee49e2e25e8939e77
Zach Siegel <zsiegel92@gmail.com>
2020-12-11 14:17:06 -0800

    viewers/chrome_viewer and a chrome/OpenFileInChrome_Page_Number.applescript utility

---
+++ viewers/chrome_viewer.py
@@ -0,0 +1,145 @@
+from urllib.parse import quote
+import subprocess as sp
+import os
+
+
+from base_viewer import BaseViewer
+from latextools_utils import get_setting
+from latextools_utils.external_command import external_command, check_output
+
+## TODO: remove defaultCmd from applescript - just return True or False, then run that default command from Python.
+## TODO: GET CHROME PATH
+## TODO: GET CURRENT FOLDER PATH (for osascript command)
+
+class ChromeViewer(BaseViewer):
+
+
+    def _get_applescript_folder(self):
+        return os.path.normpath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '..',
+                'chrome'
+            )
+        )
+
+    def _get_chrome_dir(self,escaped=False):
+        viewer_location = check_output([
+            'osascript',
+            '-e',
+            'POSIX path of '
+            '(path to app id "com.google.Chrome")'
+        ], use_texpath=False)
+        if escaped:
+            return "{viewer_location}Contents/MacOS/Google\\ Chrome".format(viewer_location=viewer_location)
+        return "{viewer_location}Contents/MacOS/Google Chrome".format(viewer_location=viewer_location)
+
+
+    def _get_browser_filepath(self,tex_line_no,tex_col_no,tex_filename,pdf_filename,zoom=100):
+        try:
+            synctex_loc = '{tex_line_no}:{tex_col_no}:{tex_filename}'.format(tex_line_no=tex_line_no,tex_col_no=tex_col_no,tex_filename=tex_filename)
+            cmd = ['/Library/TeX/texbin/synctex', 'view', '-i', synctex_loc, '-o', pdf_filename]
+            # output_text = sp.run(cmd,capture_output=True,text=True).stdout.split("\n")
+            stdout = check_output(cmd)
+            output_text = stdout.split("\n")
+            page_no = next((e.lstrip('Page:') for e in output_text if e.startswith('Page:')), 1)
+            line_no = next((e.lstrip('x:') for e in output_text if e.startswith('x:')), 1)
+            col_no = next((e.lstrip('y:') for e in output_text if e.startswith('y:')), 1)
+        except Exception as e:
+            print("ERROR IN SYNCTEX")
+            print(e)
+            page_no = 1
+            line_no = 1
+            col_no = 0
+        # print(f"{page_no = }, {line_no = }, {col_no = }")
+
+        return "file://{pdf_filename}#page={page_no}&view=FitH,{line_no}".format(pdf_filename=pdf_filename,page_no=page_no,line_no=line_no)
+
+
+    def _get_run_command(self,pdf_filename,tex_filename=None,tex_line_no=None,tex_col_no=None,zoom=100):
+        if tex_filename is None:
+            chrome_dir = self._get_chrome_dir()
+            basic_cmd =  [chrome_dir,pdf_filename]
+            return basic_cmd
+        # pdf_filename = tex_filename.rsplit(".",1)[0] + ".pdf"
+
+        finalDestination = self._get_browser_filepath(tex_line_no,tex_col_no,tex_filename,pdf_filename)
+        quotedPDFName = quote(pdf_filename)
+        scriptName= "{}/OpenFileInChrome_Page_Number.applescript".format(self._get_applescript_folder())
+        chrome_dir = self._get_chrome_dir(escaped=True)
+        defaultCmd = "{chrome_dir} '{finalDestination}'".format(chrome_dir=chrome_dir,finalDestination=finalDestination)
+        cmd = ['osascript', scriptName, finalDestination,quotedPDFName,defaultCmd]
+        return cmd
+
+
+
+    def forward_sync(self, pdf_file, tex_file, line, col, **kwargs):
+        cmd = self._get_run_command(pdf_file,tex_file,line,col)
+        print("FORWARD SYNCING CHROME COMMAND IS:")
+        print(cmd)
+        external_command(cmd, use_texpath=True)
+
+    def view_file(self, pdf_file, **kwargs):
+        tex_filename = pdf_file.rsplit(".",1)[0] + ".pdf"
+        cmd = self._get_run_command(pdf_file,tex_filename=tex_filename,tex_line_no=1,tex_col_no=1)
+        print("BASIC VIEWING CHROME")
+        external_command(cmd, use_texpath=False)
+
+
+
+    # def _test_chrome_view():
+
+    #     def get_browser_filepath(tex_line_no,tex_col_no,tex_filename,pdf_filename,zoom=100):
+
+    #         try:
+
+    #             output_text = sp.run(['synctex', 'view', '-i', f'{tex_line_no}:{tex_col_no}:{tex_filename}', '-o', pdf_filename],capture_output=True,text=True).stdout.split("\n")
+
+    #             page_no = next((e.lstrip('Page:') for e in output_text if e.startswith('Page:')), 1)
+
+    #             line_no = next((e.lstrip('x:') for e in output_text if e.startswith('x:')), 1)
+
+    #             col_no = next((e.lstrip('y:') for e in output_text if e.startswith('y:')), 1)
+
+    #         except:
+
+    #             page_no = 1
+
+    #             line_no = 1
+
+    #             col_no = 0
+
+    #         print(f"{page_no = }, {line_no = }, {col_no = }")
+
+    #         return f"file://{pdf_filename}#page={page_no}&view=FitH,{line_no}"
+
+
+
+    #     def run_applescript(tex_line_no,tex_col_no,tex_filename,zoom=100):
+
+    #         pdf_filename = tex_filename.rsplit(".",1)[0] + ".pdf"
+
+    #         finalDestination = get_browser_filepath(tex_line_no,tex_col_no,tex_filename,pdf_filename)
+
+    #         quotedPDFName = quote(pdf_filename)
+
+    #         defaultCmd = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome '{finalDestination}'"
+
+    #         result = sp.run(['osascript', "/Users/zach/Library/Application Support/Sublime Text/Packages/LaTeXTools/viewers/OpenFileInChrome_Page_Number.applescript", finalDestination,quotedPDFName,defaultCmd],capture_output=True,text=True).stdout
+
+    #         print(result)
+
+
+    #     if __name__=="__main__":
+
+    #         tex_line_no = 90
+
+    #         tex_col_no = 0
+
+    #         zoom = 100
+
+    #         tex_filename = "/Users/zach/Documents/UCLA_classes/stats203/project/final/final.tex"
+
+
+    #         run_applescript(tex_line_no,tex_col_no,tex_filename,zoom=100)
+
