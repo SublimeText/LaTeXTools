41b620b84dc63d089ebbef67a2a82b32b00ad393
deathaxe <16542113+deathaxe@users.noreply.github.com>
2025-08-10 20:20:16 +0000

    Initial support for Sioyek viewer (#1634)

---
+++ plugins/viewer/sioyek_viewer.py
@@ -0,0 +1,59 @@
+import sublime
+
+from ...latextools.utils.external_command import external_command
+from ...latextools.utils.settings import get_setting
+
+from .base_viewer import BaseViewer
+
+__all__ = ["SioyekViewer"]
+
+
+class SioyekViewer(BaseViewer):
+
+    def _run_sioyek(self, *args, **kwargs):
+        platform = sublime.platform()
+        sioyek_binary = (
+            # prefer `viewer_settings`
+            get_setting("viewer_settings", {}).get("sioyek")
+            # fallback to platform specific settings
+            or get_setting(platform, {}).get("sioyek")
+            # static binary expected to be found on $PATH
+            or "sioyek.exe"
+            if platform == "windows"
+            else "sioyek"
+        )
+
+        command = [sioyek_binary]
+
+        keep_focus = kwargs.pop("keep_focus", True)
+        if keep_focus:
+            command.append("--nofocus")
+
+        command += args
+        external_command(command, use_texpath=False, show_window=True)
+
+    def forward_sync(self, pdf_file, tex_file, line, col, **kwargs):
+        sublime_text = get_setting(sublime.platform(), {}).get("sublime_executable") or "subl"
+        self._run_sioyek(
+            "--execute-command",
+            "turn_on_synctex",
+            "--forward-search-file",
+            tex_file,
+            "--forward-search-line",
+            f"{line}",
+            "--forward-search-column",
+            f"{col}",
+            "--inverse-search",
+            f"{sublime_text} %1:%2",
+            pdf_file,
+            **kwargs
+        )
+
+    def view_file(self, pdf_file, **kwargs):
+        self._run_sioyek("--new-window", pdf_file, **kwargs)
+
+    def supports_keep_focus(self):
+        return True
+
+    def supports_platform(self, platform):
+        return True
