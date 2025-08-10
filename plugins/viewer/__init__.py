81daa4e8bec589052310a33c9c6549d3b0163ec0
deathaxe <deathaxe82@gmail.com>
2025-08-10 19:59:53 +0200

    Initial support for Sioyek viewer

--- plugins/viewer/__init__.py
+++ plugins/viewer/__init__.py
@@ -2,6 +2,7 @@ from .command_viewer import CommandViewer
 from .evince_viewer import EvinceViewer
 from .okular_viewer import OkularViewer
 from .preview_viewer import PreviewViewer
+from .sioyek_viewer import SioyekViewer
 from .skim_viewer import SkimViewer
 from .sumatra_viewer import SumatraViewer
 from .zathura_viewer import ZathuraViewer
