208205fb129c47f6229bb1a66382922d9e5cefd0
deathaxe <deathaxe82@gmail.com>
2025-08-12 18:05:28 +0200

    Refactor zathura viewer plugin
    This commit simplifies zathura viewer plugin. Tools like wmctrl or xdotool
    are no longer supported on modern wayland based Linux distros and passing
    PID to identify running instances is not required as well.

--- plugins/viewer/zathura_viewer.py
+++ plugins/viewer/zathura_viewer.py
@@ -1,4 +1,4 @@
-from shutil import which
+from __future__ import annotations
 
 from ...latextools.utils.external_command import check_output
 from ...latextools.utils.external_command import external_command
@@ -12,119 +12,38 @@ __all__ = ["ZathuraViewer"]
 
 class ZathuraViewer(BaseViewer):
 
-    def _run_zathura(self, pdf_file):
-        return external_command(
-            ["zathura", self._get_synctex_editor(), pdf_file], use_texpath=False
-        ).pid
-
-    def _get_synctex_editor(self):
-        return f"--synctex-editor-command={get_sublime_exe()} %{{input}}:%{{line}}"
-
-    def _get_zathura_pid(self, pdf_file):
-        try:
-            running_apps = check_output(["ps", "xv"], use_texpath=False)
-            for app in running_apps.splitlines():
-                if "zathura" not in app:
-                    continue
-                if pdf_file in app:
-                    return app.lstrip().split(" ", 1)[0]
-        except Exception:
-            pass
-
-        return None
-
-    def _focus_zathura(self, pid):
-        if which("xdotool") is not None:
-            try:
-                self._focus_xdotool(pid)
-                return
-            except Exception:
-                pass
-
-        if which("wmctrl") is not None:
-            try:
-                self._focus_wmctrl(pid)
-            except Exception:
-                pass
-
-    def _focus_wmctrl(self, pid):
-        window_id = None
-        try:
-            windows = check_output(["wmctrl", "-l", "-p"], use_texpath=False)
-        except Exception:
-            pass
-        else:
-            pid = f" {pid} "
-            for window in windows.splitlines():
-                if pid in window:
-                    window_id = window.split(" ", 1)[0]
-                    break
-
-        if window_id is None:
-            raise Exception("Cannot find window for Zathura")
-
-        external_command(["wmctrl", "-a", window_id, "-i"], use_texpath=False)
-
-    def _focus_xdotool(self, pid):
+    def _run_zathura(self, *args, **kwargs):
         external_command(
             [
-                "xdotool",
-                "search",
-                "--pid",
-                pid,
-                "--class",
-                "Zathura",
-                "windowactivate",
-                "%2",
+                get_setting("viewer_settings", {}).get("zathura") or "zathura",
+                f"--synctex-editor-command={get_sublime_exe()} %{{input}}:%{{line}}",
+                *args,
             ],
             use_texpath=False,
         )
+        if kwargs.get("keep_focus", True):
+            self.focus_st()
 
-    def forward_sync(self, pdf_file, tex_file, line, col, **kwargs):
-        keep_focus = kwargs.pop("keep_focus", True)
+    def _get_zathura_pid(self, pdf_file: str) -> int:
+        try:
+            running_apps = check_output(["ps", "xv"], use_texpath=False)
+            for app in running_apps.splitlines():
+                if "zathura" in app and pdf_file in app:
+                    return int(app.lstrip().split(" ", 1)[0])
+        except Exception:
+            pass
 
-        # we check for the pid here as this is only necessary if
-        # we aren't otherwise creating the window
-        pid = self._get_zathura_pid(pdf_file)
+        return 0
 
-        if pid is None:
-            pid = self._run_zathura(pdf_file)
-            if keep_focus:
-                self.focus_st()
+    def forward_sync(self, pdf_file: str, tex_file: str, line: int, col: int, **kwargs) -> None:
+        self._run_zathura("--synctex-forward", f"{line}:{col}:{tex_file}", pdf_file)
 
-        command = [
-            "zathura",
-            "--synctex-forward",
-            f"{line}:{col}:{tex_file}",
-        ]
+    def view_file(self, pdf_file: str, **kwargs) -> None:
+        if not self._get_zathura_pid(pdf_file):
+            self._run_zathura(pdf_file)
 
-        if pid is None:
-            command.append(self._get_synctex_editor())
-        else:
-            command.append(f"--synctex-pid={pid}")
-
-        command.append(pdf_file)
-
-        external_command(command, use_texpath=False)
-
-        if pid is not None and not keep_focus:
-            self._focus_zathura(pid)
-
-    def view_file(self, pdf_file, **kwargs):
-        keep_focus = kwargs.pop("keep_focus", True)
-
-        pid = self._get_zathura_pid(pdf_file)
-        if pid is None:
-            pid = self._run_zathura(pdf_file)
-            if keep_focus:
-                self.focus_st()
-        elif not keep_focus:
-            self._focus_zathura(pid)
-
-        return pid
-
-    def supports_platform(self, platform):
-        return platform == "linux"
-
-    def supports_keep_focus(self):
+    def supports_keep_focus(self) -> bool:
         return True
+
+    def supports_platform(self, platform: str) -> bool:
+        return platform == "linux"
