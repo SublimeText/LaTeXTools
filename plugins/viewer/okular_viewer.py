447e11beba9a4c067dd7cf52e3ff99774bb98cee
deathaxe <deathaxe82@gmail.com>
2025-08-12 15:13:00 +0200

    Simplify okular viewer plugin
    This commit removes various checks, which seem obsolete with current versions
    of Okular. (tested on Debian Trixie).

--- plugins/viewer/okular_viewer.py
+++ plugins/viewer/okular_viewer.py
@@ -1,8 +1,6 @@
-import time
+from __future__ import annotations
 
-from ...latextools.utils.external_command import check_output
 from ...latextools.utils.external_command import external_command
-from ...latextools.utils.settings import get_setting
 
 from .base_viewer import BaseViewer
 
@@ -12,7 +10,7 @@ __all__ = ["OkularViewer"]
 class OkularViewer(BaseViewer):
 
     def _run_okular(self, locator=None, **kwargs):
-        keep_focus = kwargs.pop("keep_focus", True)
+        keep_focus: bool = kwargs.get("keep_focus", True)
         command = ["okular", "--unique"]
         if keep_focus:
             command.append("--noraise")
@@ -20,35 +18,17 @@ class OkularViewer(BaseViewer):
             command.append(locator)
 
         external_command(command, use_texpath=False)
+        if keep_focus:
+            self.focus_st()
 
-    def _is_okular_running(self):
-        try:
-            running_apps = check_output(["ps", "xv"], use_texpath=False)
-            for app in running_apps.splitlines():
-                if "okular" not in app:
-                    continue
-                if "--unique" in app:
-                    return True
-        except Exception:
-            pass
-
-        return False
-
-    def _ensure_okular(self, **kwargs):
-        if not self._is_okular_running():
-            self._run_okular(**kwargs)
-            time.sleep(get_setting("linux", {}).get("sync_wait") or 1.0)
-
-    def forward_sync(self, pdf_file, tex_file, line, col, **kwargs):
-        self._ensure_okular()
+    def forward_sync(self, pdf_file: str, tex_file: str, line: int, col: int, **kwargs) -> None:
         self._run_okular(f"file:{pdf_file}#src:{line}{tex_file}", **kwargs)
 
-    def view_file(self, pdf_file, **kwargs):
-        self._ensure_okular()
+    def view_file(self, pdf_file: str, **kwargs) -> None:
         self._run_okular(pdf_file, **kwargs)
 
-    def supports_keep_focus(self):
+    def supports_keep_focus(self) -> bool:
         return True
 
-    def supports_platform(self, platform):
+    def supports_platform(self, platform: str) -> bool:
         return platform == "linux"
