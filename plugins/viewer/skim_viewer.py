ddbe797b64034094bca8c5e22d5231970674eed8
deathaxe <deathaxe82@gmail.com>
2025-08-12 14:21:55 +0200

    Refactor skim viewer plugin
    1. use pathlib
    2. re-arrange methods to avoid duplicate expensive calls

--- plugins/viewer/skim_viewer.py
+++ plugins/viewer/skim_viewer.py
@@ -1,6 +1,8 @@
-import os
-
+from __future__ import annotations
+import stat
 import sublime
+
+from pathlib import Path
 
 from ...latextools.utils.external_command import check_output
 from ...latextools.utils.external_command import external_command
@@ -12,24 +14,23 @@ __all__ = ["SkimViewer"]
 
 class SkimViewer(BaseViewer):
 
-    def forward_sync(self, pdf_file, tex_file, line, col, **kwargs):
-        keep_focus = kwargs.pop("keep_focus", True)
-        path_to_skim = "/Applications/Skim.app"
+    def forward_sync(self, pdf_file: str, tex_file: str, line: int, col: int, **kwargs) -> None:
+        keep_focus = kwargs.get("keep_focus", True)
+        skim_app = Path("/Applications/Skim.app")
 
-        if not os.path.exists(path_to_skim):
-            path_to_skim = check_output(
-                [
-                    "osascript",
-                    "-e",
-                    'POSIX path of (path to app id "net.sourceforge.skim-app.skim")',
-                ],
-                use_texpath=False,
+        if not skim_app.exists():
+            skim_app = Path(
+                check_output(
+                    [
+                        "osascript",
+                        "-e",
+                        'POSIX path of (path to app id "net.sourceforge.skim-app.skim")',
+                    ],
+                    use_texpath=False,
+                )
             )
 
-        command = [
-            os.path.join(path_to_skim, "Contents", "SharedSupport", "displayline"),
-            "-r",
-        ]
+        command = [str(skim_app / "Contents" / "SharedSupport" / "displayline"), "-r"]
 
         if keep_focus:
             command.append("-g")
@@ -37,24 +38,31 @@ class SkimViewer(BaseViewer):
         command += [str(line), pdf_file, tex_file]
 
         external_command(command, use_texpath=False)
+        if keep_focus:
+            self.focus_st()
 
-    def view_file(self, pdf_file, **kwargs):
-        keep_focus = kwargs.pop("keep_focus", True)
-        script_dir = os.path.join(sublime.cache_path(), "LaTeXTools", "viewer", "skim")
-        script_file = os.path.join(script_dir, "displayfile")
-        if not os.path.exists(script_file):
+    def view_file(self, pdf_file: str, **kwargs) -> None:
+        keep_focus = kwargs.get("keep_focus", True)
+        script_dir = Path(sublime.cache_path()) / "LaTeXTools" / "viewer" / "skim"
+        script_dir.mkdir(parents=True, exist_ok=True)
+        script_file = script_dir / "displayfile"
+        if not script_file.exists():
             try:
-                data = sublime.load_binary_resource(
-                    f"Packages/LaTeXTools/plugins/viewer/skim/displayfile"
+                data = (
+                    sublime.load_binary_resource(
+                        f"Packages/LaTeXTools/plugins/viewer/skim/displayfile"
+                    )
+                    .replace(b"\r\n", b"\n")
+                    .replace(b"\r", b"\n")
                 )
             except FileNotFoundError:
                 sublime.error_message(
-                    "Cannot find required scripts\n"
-                    "for 'skim' viewer in LaTeXTools package."
+                    "Cannot find required scripts\nfor 'skim' viewer in LaTeXTools package."
                 )
-            else:
-                with open(script_file, "wb") as fobj:
-                     fobj.write(data)
+                return
+
+            script_file.write_bytes(data)
+            script_file.chmod(script_file.stat().st_mode | stat.S_IXUSR)
 
         command = ["/bin/sh", script_file, "-r"]
 
@@ -64,9 +72,11 @@ class SkimViewer(BaseViewer):
         command.append(pdf_file)
 
         external_command(command, use_texpath=False)
+        if keep_focus:
+            self.focus_st()
 
-    def supports_keep_focus(self):
+    def supports_keep_focus(self) -> bool:
         return True
 
-    def supports_platform(self, platform):
+    def supports_platform(self, platform: str) -> bool:
         return platform == "osx"
